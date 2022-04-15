c701aaa126c9eaba9cead4a3cff231f2139720d2
Lleyton Gray <lea@fyralabs.com>
2022-04-15 12:51:57 -0700

    Plausable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/index.scss";
 import { AppProps } from "next/app";
 import { createTheme, NextUIProvider } from "@nextui-org/react";
+import Script from "next/script";
 
 const darkTheme = createTheme({
   type: "dark",
@@ -10,6 +11,11 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
+      <Script
+        defer
+        data-domain="fyralabs.com"
+        src="https://analytics.fyralabs.com/js/plausible.js"
+      />
     </NextUIProvider>
   );
 };
